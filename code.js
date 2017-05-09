fbb5d78e912b570bc03a899b88aad730e639e6a4
mirror <mirror12k@gmail.com>
2017-05-09 18:42:20 -0400

    - implemented a swapping mechanic

--- code.js
+++ code.js
@@ -1,5 +1,19 @@
 
+
+
+
+
 $(function ($) {
+
+
+
+	function rand_int (min, max) {
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+
+
+
 	// initialize board
 	var board_size_x = 5;
 	var board_size_y = 5;
@@ -11,39 +25,116 @@ $(function ($) {
 		}
 	}
 
+	// row-filling function which creates a new box which drops into place
+	var current_fill_row = -1;
+
+	// available box colors
+	var colors = [
+		'red',
+		'yellow',
+		'green',
+		'blue',
+		'purple',
+	];
+
 	// add proper building for the board
 	var box = $('.box');
 
 	for (var y = 0; y < board_size_y; y++) {
 		var row = $('<div class="box-row"></div>');
 		for (var x = 0; x < board_size_x; x++) {
-			row.append('<div class="box-cell box-cell-' + x + '-' + (board_size_y - y - 1)
+			var cell = $('<div class="box-cell box-cell-' + x + '-' + (board_size_y - y - 1)
 					+ '" data-cell-x="' + x + '" data-cell-y="' + (board_size_y - y - 1) + '"></div>');
+
+			cell.click((function (x, y, e) {
+				on_cell_clicked(x,y);
+			}).bind(undefined, x, board_size_y - y - 1));
+
+			row.append(cell);
 		}
 		box.append(row);
 	}
 
-	function on_clicked(x, y) {
-		board[y][x].remove();
-		board[y][x] = undefined;
+	// is the x,y coordinate in bounds?
+	function is_in_bounds(x,y) {
+		if (x < 0 || y < 0 || x >= board_size_x || y >= board_size_y)
+			return false;
+		else
+			return true;
 	}
 
+	var selected_x, selected_y;
+
+	// called when a tile is clicked
+	function on_cell_clicked(x, y) {
+		// // remove tile on click
+		// board[y][x].remove();
+		// board[y][x] = undefined;
+
+		if (selected_x === undefined) {
+			selected_x = x;
+			selected_y = y;
+		} else {
+			var dist = Math.abs(selected_x - x) + Math.abs(selected_y - y);
+			console.log('x,y: ', selected_x, selected_y, x, y);
+			console.log('distance: ', dist);
+			if (dist === 1) {
+				board[y][x].remove();
+				board[selected_y][selected_x].remove();
+
+				if (selected_x !== x) {
+					if (selected_x < x) {
+						add_animation(board[y][x], 'box-shift-left-80');
+						add_animation(board[selected_y][selected_x], 'box-shift-right-80');
+					} else {
+						add_animation(board[y][x], 'box-shift-right-80');
+						add_animation(board[selected_y][selected_x], 'box-shift-left-80');
+					}
+				} else {
+					if (selected_y < y) {
+						add_animation(board[y][x], 'box-shift-up-80');
+						add_animation(board[selected_y][selected_x], 'box-shift-down-80');
+					} else {
+						add_animation(board[y][x], 'box-shift-down-80');
+						add_animation(board[selected_y][selected_x], 'box-shift-up-80');
+					}
+				}
+
+				var swap = board[y][x];
+				board[y][x] = board[selected_y][selected_x];
+				board[selected_y][selected_x] = swap;
+
+				append_to_cell(x, y, board[y][x]);
+				append_to_cell(selected_x, selected_y, board[selected_y][selected_x]);
+			}
+
+			selected_x = undefined;
+			selected_y = undefined;
+		}
+	}
+
+	// create a new box
 	function create_box_item () {
 		var box_item = $('<div class="box-item"></div>');
 
-		box_item.click(function (e) {
-			var cell = box_item.parent();
-			var x = cell.data('cell-x');
-			var y = cell.data('cell-y');
-			
-			on_clicked(x,y);
-		});
+		var box_color = colors[rand_int(0, colors.length)];
+		console.log("debug box_color: " + box_color);
+		box_item.addClass('box-item-' + box_color);
+		box_item.attr('data-box-color', box_color);
 
 		return box_item;
 	}
 
-	// gradual row-filling function
-	var current_fill_row = -1;
+	function add_animation(item, animation) {
+		item.removeClass();
+		item.addClass('box-item box-item-' + item.data('box-color') + ' ' + animation);
+	}
+
+
+	function append_to_cell(x, y, item) {
+		box.find('.box-cell-' + x + '-' + y).append(item);
+	}
+
 	function fill_row(x) {
 
 		var y;
@@ -57,10 +148,12 @@ $(function ($) {
 		console.log("placing a new box item at ", x, y);
 		var dist = (board_size_y - y) * 80;
 		board[y][x] = create_box_item();
-		board[y][x].addClass('box-drop-' + dist);
-		box.find('.box-cell-' + x + '-' + y).append(board[y][x]);
+		add_animation(board[y][x], 'box-drop-' + dist);
+		append_to_cell(x, y, board[y][x]);
 	}
 
+
+	// update the board by dropping down floating tiles and adding new ones where necessary
 	function update_board() {
 
 		var incomplete_rows = [];
@@ -77,10 +170,10 @@ $(function ($) {
 
 							var dist = (y2 - y) * 80;
 
-							box.remove('.box-cell-' + x + '-' + y2);
-							box.find('.box-cell-' + x + '-' + y).append(board[y][x]);
-							board[y][x].removeClass();
-							board[y][x].addClass('box-item box-drop-' + dist);
+							// box.find('.box-cell-' + x + '-' + y2).remove(board[y2][x]);
+							board[y][x].remove();
+							add_animation(board[y][x], 'box-drop-' + dist);
+							append_to_cell(x, y, board[y][x]);
 
 							found = true;
 							break;
@@ -96,6 +189,7 @@ $(function ($) {
 			}
 		}
 
+		// add a block to the row if it's incomplete
 		if (incomplete_rows[current_fill_row] === true) {
 			fill_row(current_fill_row);
 		}
@@ -106,7 +200,7 @@ $(function ($) {
 		}
 	}
 
-	// trigger row filling on interval
+	// trigger updates on interval
 	setInterval(update_board, 100);
 
 });
